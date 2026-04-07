1583b8d49585aff56f8aa73439256052641183a0
Gustavo Paes <gustavo910@gmail.com>
2026-04-06 22:56:08 -0300

    CI v_005

--- components/activity-form.tsx
+++ components/activity-form.tsx
@@ -15,7 +15,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import type { AtividadeDTO, AtividadeTipo, IgrejaApi } from "@/lib/api"
+import type { AtividadeDTO, AtividadeTipo, AtividadeApi, IgrejaApi } from "@/lib/api"
 
 interface ActivityFormProps {
   churches: IgrejaApi[]
